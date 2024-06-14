19bd7da1c9b8c8dc543e0f46f3fff436669287c7
franchuk-olena <olenafranchuk@knu.ua>
2024-06-14 21:07:36 +0300

    olena

--- checkers_game.py
+++ checkers_game.py
@@ -113,10 +113,4 @@ class CheckersGame:
             game = CheckersGame(root)
             root.mainloop()
 
-    def check_winner(self):
-        if self.red_captured == 12:
-            messagebox.showinfo("Гра завершена", "Сині перемогли!")
-            self.end_game()
-        elif self.blue_captured == 12:
-            messagebox.showinfo("Гра завершена", "Червоні перемогли!")
-            self.end_game()
+    
